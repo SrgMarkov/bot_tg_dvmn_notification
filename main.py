6874db4f19f703e3e5077cf8e9820767b6ce3e21
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2023-08-30 12:27:15 +0400

    Add textwrap.dedent

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
+from textwrap import dedent
 from telegram import Bot
 import requests
 from dotenv import load_dotenv
 
-
 LONG_POLLING_URL = 'https://dvmn.org/api/long_polling/'
-
 
 if __name__ == '__main__':
     load_dotenv()
@@ -27,18 +26,18 @@ if __name__ == '__main__':
             elif devman_server_response['status'] == 'found':
                 params['timestamp_to_request'] = devman_server_response['last_attempt_timestamp']
                 review_status = devman_server_response['new_attempts'][0]
-                lesson_tittle_text = f'У Вас проверили работу "{review_status["lesson_title"]}"\n' \
-                                     f'{review_status["lesson_url"]}\n\n'
+                lesson_tittle_text = f'''\
+                                     У Вас проверили работу "{review_status["lesson_title"]}" 
+                                     {review_status["lesson_url"]}
+                                     
+                                     '''
                 if review_status['is_negative']:
                     negative_text = 'К сожалению, в работе нашлись ошибки.'
-                    bot.send_message(chat_id=chat_id, text=lesson_tittle_text + negative_text)
+                    bot.send_message(chat_id=chat_id, text=dedent(lesson_tittle_text) + negative_text)
                 else:
                     positive_text = 'Преподавателю всё понравилось. Можно приступать к следующему уроку!'
-                    bot.send_message(chat_id=chat_id, text=lesson_tittle_text + positive_text)
+                    bot.send_message(chat_id=chat_id, text=dedent(lesson_tittle_text) + positive_text)
         except requests.exceptions.ReadTimeout as error:
             print(f'Ошибка: {error}')
         except requests.exceptions.ConnectionError as error:
             print(f'Прервано соединение: {error}')
-
-
-
